6e2921cc8c3f5f3f9e6d0678aadbda0a7567f266
Eric Tsai <etsai@treetopllc.com>
2020-07-20 17:50:07 -0700

    WIP

--- src/election_anomaly/db_routines/__init__.py
+++ src/election_anomaly/db_routines/__init__.py
@@ -484,7 +484,7 @@ def truncate_table(session, table_name):
     return
 
 
-def get_input_options(session, input):
+def get_input_options(session, input, verbose):
     """Returns a list of response options based on the input"""
     # input comes as a pythonic (snake case) input, need to 
     # change to match DB table naming format
@@ -505,19 +505,39 @@ def get_input_options(session, input):
     elif search_str == 'Candidate':
         column_name = 'BallotName'
         table_search = True
+    # TODO: do we need a subdivision_type?
     else:
         search_str = search_str.lower()
         table_search = False
 
-    if table_search:
-        result = session.execute(f'SELECT "{column_name}" FROM "{search_str}";')
-        return [r[0] for r in result]
+    if not verbose:
+        if table_search:
+            result = session.execute(f'SELECT "{column_name}" FROM "{search_str}";')
+            return [r[0] for r in result]
+        else:
+            result = session.execute(f' \
+                SELECT "Name" FROM "ReportingUnit" ru \
+                JOIN "ReportingUnitType" rut on ru."ReportingUnitType_Id" = rut."Id" \
+                WHERE rut."Txt" = \'{search_str}\'')
+            return [r[0] for r in result]
     else:
-        result = session.execute(f' \
-            SELECT "Name" FROM "ReportingUnit" ru \
-            JOIN "ReportingUnitType" rut on ru."ReportingUnitType_Id" = rut."Id" \
-            WHERE rut."Txt" = \'{search_str}\'')
-        return [r[0] for r in result]
+        if search_str == 'BallotMeasureContest':
+            result = session.execute(f'''
+                SELECT  ru."Id" AS parent_id, c."Name" AS name, rut."Txt" AS type
+                FROM    "BallotMeasureContest" c
+                        JOIN "ReportingUnit" ru ON c."ElectionDistrict_Id" = ru."Id"
+                        JOIN "ReportingUnitType" rut ON ru."ReportingUnitType_Id" = rut."Id"
+            ''')
+        elif search_str == 'CandidateContest':
+            result = session.execute(f'''
+                SELECT  ru."Id" AS parent_id, c."Name" AS name, rut."Txt" AS type
+                FROM    "CandidateContest" c
+                        JOIN "Office" o ON c."Office_Id" = o."Id"
+                        JOIN "ReportingUnit" ru ON o."ElectionDistrict_Id" = ru."Id"
+                        JOIN "ReportingUnitType" rut ON ru."ReportingUnitType_Id" = rut."Id"
+            ''')
+        else:
+           pass 
 
 
 def get_datafile_info(session, results_file):
